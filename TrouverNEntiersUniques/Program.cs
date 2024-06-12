6ab7d1e4d8da3ad4aede04333a24cf6d565a0dc7
AnSimonyan <anna.simona1976@gmail.com>
2024-06-12 11:41:25 +0300

    First version

--- TrouverNEntiersUniques/Program.cs
+++ TrouverNEntiersUniques/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.Write("Please enter a number:");
+            string numberEnteredByUserAsString = Console.ReadLine();
+            string resultString="[";
+            int numberEnteredByUser = Convert.ToInt16(numberEnteredByUserAsString);
+            int limitForLoop = numberEnteredByUser / 2;
+            int[] arrayTheNumbers = new int[numberEnteredByUser];
+            
+
+            for (int i = numberEnteredByUser-1; i > limitForLoop; i--) 
+            {
+                arrayTheNumbers[i] = -i-1;
+            }
+            for (int i = 0; i < limitForLoop; i++)
+            {
+                arrayTheNumbers[i] = -arrayTheNumbers[limitForLoop+(i+1)];
+            }
+            if (numberEnteredByUser / 2 != 0)
+            {
+                arrayTheNumbers[limitForLoop] = arrayTheNumbers[0]-1;
+                arrayTheNumbers[0] = 1;
+            }
+
+            for (int i = 0; i < numberEnteredByUser; i++)
+            {
+                resultString = resultString +Convert.ToString(arrayTheNumbers[i]) +",";
+            }
+            Console.WriteLine(resultString.Remove(resultString.Length-1, 1)+"]");
         }
     }
 }
